9f165140fc5fb927a61937262ffd61baf16e3bae
haunt07 <fachri111.gmail.com>
2019-04-07 06:40:27 +0700

    Update Comment

--- examples/Moving_Average/Moving_Average.ino
+++ examples/Moving_Average/Moving_Average.ino
@@ -4,26 +4,26 @@
 
 #include <mv.h>
 
-simple_moving_average sensor_1(3); //number of time periods is 3
-simple_moving_average sensor_2(5); //number of time periods is 5
+simple_moving_average sensor_1(3); // number of time periods is 3
+simple_moving_average sensor_2(5); // number of time periods is 5
 
 void setup()
 {
-    //example for simple moving average 
+    // example for simple moving average
     Serial.begin(9600);
     Serial.println("Sensor 1\nValue\tResult");
     for (int i = 0; i <= 10; i++)
     {
         Serial.print(i);
         Serial.print("\t\t");
-        Serial.println(sensor_1.process(i)); //add new value and return the result
+        Serial.println(sensor_1.process(i)); // add new value and return the result
     }
     Serial.println("\nSensor 2\nValue\tResult");
     for (int i = 0; i <= 10; i++)
     {
         Serial.print(i);
         Serial.print("\t\t");
-        Serial.println(sensor_2.process(i)); //add new value and return the result
+        Serial.println(sensor_2.process(i)); // add new value and return the result
     }
 }
 
